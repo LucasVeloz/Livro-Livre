9316ae3d67478ac74dd4edfbac0355f0e8a4de06
LucasVeloz <lurucell@gmail.com>
2024-08-01 10:59:46 -0300

    fix: removing root layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import type { AppProps } from 'next/app'
 import './globals.css'
 import { Wrapper } from '@/components'
 
+
+// import { Inter } from "next/font/google";
+
+
+// const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Wrapper>
