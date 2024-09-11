591e2a7b10f50941f6b2d7af20418be4cb2a5460
LucasVeloz <lurucell@gmail.com>
2024-09-11 19:29:40 -0300

    fix: icon of home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 
 import { Menu } from "@/components/menu";
 import { Search } from "@/components/search";
-import { BookCopyIcon, FolderInputIcon, User2Icon, UserPlus2Icon } from "lucide-react";
+import { BookCopyIcon, User2Icon, UserPlus2Icon } from "lucide-react";
 import Link from "next/link";
 
 export const routes = [
@@ -34,9 +34,9 @@ export default function Page() {
     <main className="flex flex-col w-full min-h-screen">
       <Search />
       <div className="flex flex-col gap-5 pt-6 px-7 mt-[5.25rem] pb-[7rem] w-full">
-        {routes.map(({ name, path }, index) => (
+        {routes.map(({ name, path, Icon }, index) => (
           <Link key={name + index} href={path} className="w-full px-8 py-5 bg-custom-white rounded-lg flex items-center gap-4 border border-custom-lightGray shadow-base text-2xl font-bold text-primary">
-          <UserPlus2Icon size={32} />
+          {Icon}
           {name}
         </Link>
         ))}
